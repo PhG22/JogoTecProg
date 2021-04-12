9d6d6c09fa985d75c5814b8c41b575df51695d38
PhG22 <57491645+PhG22@users.noreply.github.com>
2021-04-12 18:20:16 -0300

    Add files via upload

---
+++ Player.cpp
@@ -0,0 +1,71 @@
+#include "Player.h"
+
+Player::Player(Vetor2F pos): Desenhavel(pos, Vetor2F(), "player.png")
+{}
+
+Player::~Player()
+{}
+
+void Player::inicializar(GerenciadorGrafico& janela, GerenciadorEventos& gEvent)
+{
+	janela.carregarTextura(caminho);
+
+	chaveListener = gEvent.addListenerTeclado([this](const Event ev) {tratarEvento(ev); });
+}
+
+void Player::atualizar(float t)
+{
+	position.x = position.x + (v.x * t);
+	position.y = position.y + (v.y * t);
+
+}
+
+void Player::desenhar(GerenciadorGrafico& janela)
+{
+	janela.centralizar(position);
+
+	janela.desenhar(caminho, position);
+}
+
+void Player::tratarEvento(const sf::Event& ev)
+{
+	if (ev.type == Event::KeyPressed) {
+		switch (ev.key.code)
+		{
+			case Keyboard::Key::D:
+				v.x = 300;
+				break;
+			case Keyboard::Key::A:
+				v.x = -300;
+				break;
+			case Keyboard::Key::W:
+				v.y = -300;
+				break;
+			case Keyboard::Key::S:
+				v.y = 300;
+				break;
+			default:
+				break;
+		}
+	}
+
+	if (ev.type == Event::KeyReleased) {
+		switch (ev.key.code)
+		{
+			case Keyboard::Key::D:
+				v.x = 0;
+				break;
+			case Keyboard::Key::A:
+				v.x = 0;
+				break;
+			case Keyboard::Key::W:
+				v.y = 0;
+				break;
+			case Keyboard::Key::S:
+				v.y = 0;
+				break;
+			default:
+				break;
+		}
+	}
+}
--- Player.h
+++ Player.h
@@ -2,5 +2,16 @@
 #include "Desenhavel.h"
 class Player : public Desenhavel
 {
+private:
+	unsigned int chaveListener;
+
+public:
+
+	Player(Vetor2F pos);
+	~Player();
+	void inicializar(GerenciadorGrafico& janela, GerenciadorEventos& gEvent);
+	void atualizar(float t);
+	void desenhar(GerenciadorGrafico& janela);
+	void tratarEvento(const Event& ev);
 };
 
