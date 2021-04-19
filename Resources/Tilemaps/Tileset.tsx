c7dea9f4c53b925dac8360f6122cd2964b003a02
PhG22 <57491645+PhG22@users.noreply.github.com>
2021-04-19 19:08:16 -0300

    Add files via upload

---
+++ Resources/Tilemaps/Tileset.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Tileset" tilewidth="32" tileheight="32" tilecount="17" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="13">
+  <image width="32" height="32" source="../Textures/arbusto.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="../Textures/cacto.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="../Textures/caixa.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../Textures/pedra.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="../Textures/placa1.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="../Textures/placa2.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="../Textures/placa3.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="../Textures/placa4.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="../Textures/spike.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="../Textures/tile agua.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="../Textures/tile ground.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="../Textures/tile lava.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="../Textures/vaso.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="../Textures/vazioV1.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="../Textures/vazioV2.png"/>
+ </tile>
+ <tile id="28">
+  <image width="32" height="32" source="../Textures/moeda.png"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="32" source="../Textures/vida.png"/>
+ </tile>
+</tileset>
